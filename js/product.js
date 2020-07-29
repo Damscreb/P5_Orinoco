1992ff3db31e0e76283f5f32203398c8d517502b
Damscreb <damien.crebassa@free.fr>
2020-07-29 15:50:50 +0200

    Page produit terminée avec script !!

--- js/product.js
+++ js/product.js
@@ -1,44 +1,150 @@
+// Création d'une classe Produit qui nous permettra de récupérer les données des produits 
+// que le client a ajouté à son panier.
+
+class Produit {
+    constructor(id, name, quantity, color, price, imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.color = color;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+}
+
 // DONC, le but est de personnaliser la page en fonction de l'id stocké dans local storage
 // On veut le nom, l'image, le prix, et les différentes couleurs aussi :)
-
 let teddieName = document.getElementById('name');
 let teddieImg = document.getElementById('picture');
 let teddiePrice = document.getElementById('price');
 
 let colorForm = document.getElementById('color-choice');
+
 let newInput = document.createElement('input');
-    newInput.name = 'color';
-    newInput.type = 'radio';
+newInput.name = 'color';
+newInput.type = 'radio';
+newInput.required = true;
+
 let newLabel = document.createElement('label');
+newLabel.className += 'ml-2 pb-1';  // On aère un peu le formulaire
+
 let newBr = document.createElement('br');
+
+let confirmation = document.getElementById('confirmation');
 
 // Premièrement, récupérer les données du Teddy grâce à l'id
 // Pour ce faire on va appeller l'API avec l'ID du produit voulu
 
-console.log(localStorage);
-
 fetch(`http://localhost:3000/api/teddies/${localStorage.id}`)
-    .then(function(response) {
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data) {
-        console.log(data)
+    .then(function (data) {
+
         // Récupération dans l'API pour afficher le nom, l'image et le prix du Teddy sélectionné
         teddieName.innerHTML = data.name;
         teddieImg.src = data.imageUrl;
-        data.price = data.price/100;
-        teddiePrice.textContent = `Prix : ${data.price},00 €`;  
-        for (let j=0; j<data.colors.length; j++) {     
-            // Boucle pour afficher les couleurs dispo
+        data.price = data.price / 100;
+        teddiePrice.textContent = `Prix : ${data.price},00 €`;
 
-            colorForm.appendChild(newInput.cloneNode(true)).id= `color-${j}`;
-            colorForm.appendChild(newLabel.cloneNode(true)).id= `color${j}`
+        // Boucle pour afficher les couleurs dispo
+        for (let j = 0; j < data.colors.length; j++) {
+
+            colorForm.appendChild(newInput.cloneNode(true)).id = `color-${j}`;
+            document.getElementById(`color-${j}`).value = data.colors[j];
+
+            colorForm.appendChild(newLabel.cloneNode(true)).id = `color${j}`
             document.getElementById(`color${j}`).innerHTML = data.colors[j];
             document.getElementById(`color${j}`).htmlFor = `color-${j}`;
-            colorForm.appendChild(newBr.cloneNode(true));         
+
+            colorForm.appendChild(newBr.cloneNode(true));
+
         }
-        
+
+        // Mise en page plus aérée
+        colorForm.appendChild(newBr);
+
+        // Ajoutons un addEventListener au clic du bouton d'envoi du formulaire
+        document.querySelector('#product-form').addEventListener('submit', function (e) {
+
+            // Désactiver l'action par défaut 
+            e.preventDefault();
+
+            // Je récupère les données du produit dont il est question, et je les range dans une classe Produit
+            // {id, name, quantity, color, price, image}
+            let product = new Produit(`${localStorage.id}`, `${data.name}`, `${document.getElementById('quantity').value}`, `${document.querySelector('input[name=color]:checked').value}`, `${data.price}`, `${data.imageUrl}`);
+
+            // Vérifier si le localStorage n'est pas vide
+            if (localStorage.getItem('products') !== null) {
+
+                let productArray = JSON.parse(localStorage.getItem('products'));    // On récupère ce qu'il y a dans le local storage, et on met ça sous forme de tableau
+                console.log(productArray.length);
+
+                // Vérification à faire si on a déjà le produit dans le local storage
+                for (let i=0; i<productArray.length; i++) {
+
+                    if (productArray[i].id === localStorage.id) {   // Vérification si l'idientifiant du produit qu'on souhaite ajouter n'est pas dans le tableau de produits
+                        
+                        // parseInt() transforme un string en nombre !! On peut donc additionner nos deux valeurs!
+                        productArray[i].quantity = parseInt(product.quantity) + parseInt(productArray[i].quantity); // On ne change que la quantité
+
+                        // Afficher une alerte pour dire que le produit a été ajouté dans le panier
+                        confirmation.innerHTML =`${data.name} a bien été ajouté au panier (x${product.quantity})
+                                                <br/>
+                                                Total : ${productArray[i].quantity}`;
+                        confirmation.hidden = false;
+
+                        // Enregistrons les données
+                        let productArrayString = JSON.stringify(productArray);  // On convertit le tableau de produits en string 
+                        localStorage.setItem('products', productArrayString);   // On le sauvegarde dans le localStorage
+                        console.log('Qté produit mise à jour');
+
+                        // Stoppons la boucle for
+                        i = productArray.length; 
+                    }                   
+
+                    if (i === (productArray.length)-1 && productArray[i].id !== localStorage.id) {  // Si on arrive au bout de la longueur du tableau, le produit est donc nouveau --> on l'ajoute
+                        
+                        // On ajoute une nouvelle entrée dans le tableau 
+                        productArray.push(product);                    
+                        
+                        // Enregistrons les données
+                        let productArrayString = JSON.stringify(productArray);      // On convertit ce tableau de produits en string 
+                        localStorage.setItem('products', productArrayString);       // On le sauvegarde dans le localStorage
+                        console.log('Produit non existant, panier non vide');
+
+                        // Afficher une alerte pour dire que le produit a été ajouté dans le panier
+                        confirmation.innerHTML = `${data.name} a bien été ajouté au panier (x${product.quantity})
+                        <br/>
+                        Total : ${product.quantity}`;
+                        confirmation.hidden = false;
+
+                        // Stoppons la boucle for
+                        i = productArray.length; 
+                    }
+                }
+            }                    
+                
+            else {   // Si le localStorage est vide :
+
+                console.log('localStorage vide, ajout du produit');
+                let productArray = [product];                               // On l'ajoute directement dans le tableau de produits
+                let productArrayString = JSON.stringify(productArray);      // On convertit ce tableau de produits en string 
+                localStorage.setItem('products', productArrayString);       // On le sauvegarde dans le localStorage (qui n'accepte que des strings)
+                console.log(localStorage);
+
+                // Afficher une alerte pour dire que le produit a été ajouté dans le panier
+                confirmation.innerHTML =`${data.name} a bien été ajouté au panier (x${product.quantity})
+                                        <br/>
+                                        Total : ${product.quantity}`;
+                confirmation.hidden = false;
+            }
+
+            
+        });
+
+
     })
-    .catch(function(err) {
+    .catch(function (err) {
         console.log('Fetch problem: ' + err.message);
     });
--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 // Données pour création html
+// Facon de procéder longue et compliquée, on fera mieux sur la page panier
+//localStorage.clear();
 
 let cards = document.getElementById('cards');
 let firstDiv = document.createElement('div');
     firstDiv.className ='col-9 col-md-6 col-lg-4 mx-auto mt-5';
+
 let secondDiv = document.createElement('div');
     secondDiv.className = 'card mb-4 mb-lg-0 border-secondary shadow text-center';
+
 let img = document.createElement('img');
     img.className = 'card-img-top furniture-height';
+
 let thirdDiv = document.createElement('div');
     thirdDiv.className = 'card-body';
+
 let h4 = document.createElement('h4');
     h4.className = 'card-title';
+
 let p = document.createElement('p');
     p.className = 'description';
+
 let a = document.createElement('a');
     a.className = 'card-text';
     a.href='./pages/product.html'
+
 let button = document.createElement('button');
     button.type ='button';
     button.className = 'btn btn-info btn-lg';
@@ -45,17 +54,22 @@ fetch('http://localhost:3000/api/teddies')
 
                 // On ajoute une premiere div a qui on donne ses classes
             cards.appendChild(firstDiv.cloneNode(true)).className += ` div1-${i}`;
+
                 // On sélectionne la bonne nouvelle div (class) pour y ajouter une seconde div a qui on donne ses classes
             document.querySelector(`div.div1-${i}`).appendChild(secondDiv.cloneNode(true)).className += ` div2-${i}`;
+
                 // Ajoutons notre image avec ses classes
             document.querySelector(`div.div2-${i}`).appendChild(img.cloneNode(true)).className += ` img-${i}`;
+
                 // Ajoutons aussi une toisième div à la deuxième div
             document.querySelector(`div.div2-${i}`).appendChild(thirdDiv.cloneNode(true)).className += ` div3-${i}`;
+
                 // Ajoutons à cette troisième div un h4 un p et un a
             document.querySelector(`div.div3-${i}`).appendChild(h4.cloneNode(true)).className += ` h4-${i}`;
             document.querySelector(`div.div3-${i}`).appendChild(p.cloneNode(true)).className += ` description-${i}`;
             document.querySelector(`div.div3-${i}`).appendChild(a.cloneNode(true)).className += ` link-${i}`;
-                // Ajoutons à a un bouton et un href
+            
+                // Ajoutons à <a></a> un bouton et un href
             document.querySelector(`a.link-${i}`).appendChild(button.cloneNode(true));
 
 
@@ -64,11 +78,10 @@ fetch('http://localhost:3000/api/teddies')
             document.querySelector(`p.description-${i}`).textContent = data[i].description;
             document.querySelector(`img.img-${i}`).src = data[i].imageUrl;
             teddiesId.push(data[i]._id);
-
         }
+
         // Récupération de mes boutons dans une NodeList
         let detailBtnNl = document.querySelectorAll(".btn-info");
-        console.log(detailBtnNl);
 
         // Sauvegarde dans localStorage de l'ID concerné au clic du btn détail
         // La boucle permet de voyager dans ma NodeList pour ajouter un addEventListener à chaque composant
